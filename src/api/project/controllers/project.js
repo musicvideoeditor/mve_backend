201cfa3d86fc9839d2bcacb212eb4d70eaa5dd93
DezyNation <87603422+DezyNation@users.noreply.github.com>
2025-01-12 00:25:47 +0530

    create project apis modified

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -1,9 +1,29 @@
-'use strict';
+"use strict";
 
 /**
  * project controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::project.project');
+module.exports = createCoreController("api::project.project", ({ strapi }) => ({
+  create: async (ctx) => {
+    try {
+      const { name, description } = ctx.request.body;
+
+      const project = await strapi.documents("api::project.project").create({
+        data: {
+          name: name,
+          description: description,
+          author: {
+            connect: [ctx.state.user.id],
+          },
+        },
+      });
+
+      return (ctx.body = project);
+    } catch (error) {
+      ctx.internalServerError(error);
+    }
+  },
+}));
